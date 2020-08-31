c6e75eb93c7d1764ade0d42ce573631799644957
Santiago Gutiérrez Orjuela <santiago.gutierrez02@correo.usa.edu.co>
2020-08-30 23:21:09 -0500

    Creacion clase Tarjeta y constructor

---
+++ src/Tarjeta.java
@@ -0,0 +1,12 @@
+public class Tarjeta {
+    Cuenta cuenta;
+    int numemroTarjeta;
+    int numeroccv;
+
+    public Tarjeta(Cuenta cuenta, int numemroTarjeta, int numeroccv) {
+        this.cuenta = cuenta;
+        this.numemroTarjeta = numemroTarjeta;
+        this.numeroccv = numeroccv;
+    }
+
+}
