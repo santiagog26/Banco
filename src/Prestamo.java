7a63f8bbb940e14a9d84c6969ce8857b42a97620
valeriabermudez01 <valeria.bermudez01@correo.usa.edu.co>
2020-08-30 22:49:10 -0500

    Se agrega clase Prestamo

---
+++ src/Prestamo.java
@@ -0,0 +1,14 @@
+public class Prestamo {
+    double cantidad;
+    int noCuotas;
+    double interes;
+    double valorCuota;
+
+    public Prestamo(double cantidad, int noCuotas, double interes, double valorCuota) {
+        this.cantidad = cantidad;
+        this.noCuotas = noCuotas;
+        this.interes = interes;
+        this.valorCuota = valorCuota;
+    }
+}
+
